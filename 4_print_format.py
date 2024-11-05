b5417182e95628ae65502994e2367d8729360b8c
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-11-05 14:18:52 +0530

    Create 4_print_format.py

---
+++ 4_print_format.py
@@ -0,0 +1,9 @@
+First_Name = "Madhava Reddy"
+Last_Name = "Bonam"
+City = "Hyderabad"
+
+# different way of using print
+print("Hi all, I am", First_Name,Last_Name, "From", City)
+print("my first name is {} and last name is {}".format(First_Name, Last_Name))
+print(f"My name is {First_Name} {Last_Name}")
+print("Hi all, my name is "+First_Name+" "+Last_Name) # CONCATINATION
