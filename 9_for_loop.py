402d9cb913ce4e6d129871a5f5ebd9a7c276bcee
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-11-05 18:09:26 +0530

    Create 9_for_loop.py

---
+++ 9_for_loop.py
@@ -0,0 +1,18 @@
+# for loop for string
+print("For loop for string:")
+name = "madhava"
+for i in name:
+    print(f"The character of name:",i)
+
+# for loop for tuple
+print("For loop for tuple:")
+movies = ("pushpa", "kgf", "rrr", "kalki", "bahubali")
+for mv_name in movies:
+    print(f"The movie {mv_name} is blocker bluster hit")
+
+# for loop in list
+print("for loop in list:")
+heros = ["prabhas", "ntr", "ram charan", "allu arjun", "nani", "Ram"]
+for actor in heros:
+    print(f"{actor} was from tollywood")
+
