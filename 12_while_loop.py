7ffaadc169cf363635fce804156a04a68b70d96e
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-11-05 18:10:20 +0530

    Create 12_while_loop.py

---
+++ 12_while_loop.py
@@ -0,0 +1,14 @@
+# while loop
+x = 1
+while x <= 15:
+    print(f"The value of x is {x}")
+    x = x + 1
+print("After the loop")
+
+# While loop another example
+import time
+z = 5
+while True:
+    print(f"the value of z is {z}")
+    z = z * 2
+    time.sleep(3)
