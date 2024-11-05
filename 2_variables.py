516d6878e25b0bcc0881dcba1c596a598061a38f
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-11-05 13:04:37 +0530

    Create 2_variables.py

---
+++ 2_variables.py
@@ -0,0 +1,15 @@
+var1 = "Bonam Madhava Reddy"
+var2 = "HYD"
+var3 = "DevOps"
+var4 = 3
+
+print(f"Hi, I am {var1} from {var2}. I have {var4} years of experience in {var3}")
+
+# assiging and printing variable
+x=20
+print("value of x is", x)
+
+# To know type of variabe
+print (type(x))
+print(type(var1))
+print(type(var4))
