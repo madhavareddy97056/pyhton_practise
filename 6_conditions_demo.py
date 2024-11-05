bf6c1b7d2abf99e3df705157aad5c4ccccf33b91
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-11-05 17:00:05 +0530

    Create 6_conditions_demo.py

---
+++ 6_conditions_demo.py
@@ -0,0 +1,32 @@
+# if condition
+a = 200
+
+if a < 100:
+    print("inside if block")
+    print(f"{a} is lessthan 100")
+
+
+# if/else condition
+a = 50
+
+if a < 100:
+    print("In if block")
+    print(f"{a} is lessthan 100")
+else:
+    print("In else block")
+    print(f"{a} is greaterthan 100")
+
+
+# if/elif/else condition
+a = 200
+
+if a < 100:
+    print("In if block")
+    print(f"{a} is lessthan 100")
+elif a ==100:
+    print("In elif block")
+    print(f"{a} is equal to 100")
+else:
+    print("In else block")
+    print(f"{a} is greaterthan 100")
+
