fcb0052f083ea6b032ee23b1ce36cdd22cf1ef38
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-11-05 12:47:37 +0530

    Create 1_comments.py

---
+++ 1_comments.py
@@ -0,0 +1,9 @@
+# This is sinle line comment
+print ("This is single line comment")
+
+
+"""
+This is multi line comment
+we are write commnet in more than one line
+"""
+print ("This multiline commnet")
